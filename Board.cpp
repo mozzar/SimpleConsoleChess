c465bfa1df9b65e55cf3139bc8a5cec28370dd6f
mozzar <realmozzar@gmail.com>
2022-03-28 17:32:28 +0200

    first commit

---
+++ Board.cpp
@@ -0,0 +1,20 @@
+//
+// Created by marcin on 28.03.2022.
+//
+
+#include "Board.h"
+
+
+Board::Board() {
+    for(int place=0; place < 8; place++){
+
+    }
+}
+
+void Board::play() {
+
+}
+
+void Board::print_game_state() {
+
+}
---
+++ Board.h
@@ -0,0 +1,9 @@
+//
+// Created by marcin on 28.03.2022.
+//
+
+#ifndef SIMPLECONSOLECHESS_BOARD_H
+#define SIMPLECONSOLECHESS_BOARD_H
+
+
+#endif //SIMPLECONSOLECHESS_BOARD_H
